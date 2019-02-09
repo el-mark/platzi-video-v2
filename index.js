0e2ba7eabdc94ec78c18a82b223bdca719389751
Mark <mharmsenr@gmail.com>
2019-02-09 13:06:15 -0300

    First commit of Platzi Video

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app = document.getElementById('app');
+
+const elemento = <div>Como puedo hacer para no estar solo?</div>;
+
+ReactDOM.render(elemento, app);
