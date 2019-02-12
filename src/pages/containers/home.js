6e4e8e6d3bb4c5971fd5180cde7076605494af2c
Mark Harmsen <mharmsenr@gmail.com>
2019-02-12 18:19:52 -0300

    Adjust components to the new structure so it renders the playlists.

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import HomeLayout from '../components/home-layout'
+import HomeLayout from '../components/home-layout';
+import Categories from '../../categories/components/categories';
 
 class Home extends Component {
     render() {
+        console.log(this.props.data.categories)
         return (
-            <HomeLayout />
+            <HomeLayout>
+                <Categories categories={this.props.data.categories} />
+            </HomeLayout>
         )
     }
 }
--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,57 +1,13 @@
 import React from 'react';
 import Media from './media';
 import './playlist.css';
-import Play from '../../icons/components/play';
-import Pause from '../../icons/components/pause';
-import Volume from '../../icons/components/volume';
-import FullScreen from '../../icons/components/full-screen';
 
 function Playlist(props) {
-    const playlistD = props.data.categories[0].playlist
-    const playlistP = props.data.categories[1].playlist
-    const playlistR = props.data.categories[2].playlist
-    console.log(props.data)
     return (
         <div>
-            <span>Lo mejor de la semana</span>
-            <h2>Destacados</h2>
-            <div className="Playlist">
-                <Play 
-                    size={100}
-                    color="red"
-                />
-                <Pause 
-                    size={30}
-                    color="blue"
-                />
-                <Volume
-                    size={150}
-                    color="gold"
-                />
-                <FullScreen 
-                    size={33}
-                    color="pink"
-                />
-                {
-                    playlistD.map((item) => {
-                        return <Media {...item} key={item.id}/>
-                    })
-                }
-            </div>
-            <span>Lo mejor para compartir</span>
-            <h2>Para Programar</h2>
             <div className="Playlist">
                 {
-                    playlistP.map((item) => {
-                        return <Media {...item} key={item.id}/>
-                    })
-                }
-            </div>
-            <span>Si no te queda de otra</span>
-            <h2>Regueton</h2>
-            <div className="Playlist">
-                {
-                    playlistR.map((item) => {
+                    props.playlist.map((item) => {
                         return <Media {...item} key={item.id}/>
                     })
                 }
