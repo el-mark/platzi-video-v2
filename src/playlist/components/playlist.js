bda44629a5b24c4191dafa898d59da8caf1ab5b9
Mark Harmsen <mharmsenr@gmail.com>
2019-02-12 14:42:50 -0300

    Change Playlist component to a functional component

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,45 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Media from './media';
 import './playlist.css';
 
-class Playlist extends Component {
-    render() {
-        const playlistD = this.props.data.categories[0].playlist
-        const playlistP = this.props.data.categories[1].playlist
-        const playlistR = this.props.data.categories[2].playlist
-        console.log(this.props.data)
-        return (
-            <div>
-                <span>Lo mejor de la semana</span>
-                <h2>Destacados</h2>
-                <div className="Playlist">
-                    {
-                        playlistD.map((item) => {
-                            return <Media {...item} key={item.id}/>
-                        })
-                    }
-                </div>
-                <span>Lo mejor para compartir</span>
-                <h2>Para Programar</h2>
-                <div className="Playlist">
-                    {
-                        playlistP.map((item) => {
-                            return <Media {...item} key={item.id}/>
-                        })
-                    }
-                </div>
-                <span>Si no te queda de otra</span>
-                <h2>Regueton</h2>
-                <div className="Playlist">
-                    {
-                        playlistR.map((item) => {
-                            return <Media {...item} key={item.id}/>
-                        })
-                    }
-                </div>
+function Playlist(props) {
+    const playlistD = props.data.categories[0].playlist
+    const playlistP = props.data.categories[1].playlist
+    const playlistR = props.data.categories[2].playlist
+    console.log(props.data)
+    return (
+        <div>
+            <span>Lo mejor de la semana</span>
+            <h2>Destacados</h2>
+            <div className="Playlist">
+                {
+                    playlistD.map((item) => {
+                        return <Media {...item} key={item.id}/>
+                    })
+                }
             </div>
-        )
-    }
+            <span>Lo mejor para compartir</span>
+            <h2>Para Programar</h2>
+            <div className="Playlist">
+                {
+                    playlistP.map((item) => {
+                        return <Media {...item} key={item.id}/>
+                    })
+                }
+            </div>
+            <span>Si no te queda de otra</span>
+            <h2>Regueton</h2>
+            <div className="Playlist">
+                {
+                    playlistR.map((item) => {
+                        return <Media {...item} key={item.id}/>
+                    })
+                }
+            </div>
+        </div>
+    )
 }
 
 export default Playlist;
