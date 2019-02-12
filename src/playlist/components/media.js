8209cde141b95f36c83364d0a6cab717745b8cb1
Mark Harmsen <mharmsenr@gmail.com>
2019-02-12 12:00:11 -0300

    Add new component: Playlist. And inster Media as a List of components.

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import './media.css'
 
@@ -20,26 +20,26 @@ class Media extends Component {
         })
     }
     render() {
-        const styles = {
-            container: {
-                color: '#44546b',
-                cursor: 'poiner',
-                width: 260,
-                border: '1px solid red'
-            }
-        }
+        // const styles = {
+        //     container: {
+        //         color: '#44546b',
+        //         cursor: 'poiner',
+        //         width: 260,
+        //         border: '1px solid red'
+        //     }
+        // }
         return (
             <div className="Media" onClick={this.handleClick}>
                 <div className="Media-cover">
                     <img 
                         className="Media-image"
-                        src={this.props.image}
+                        src={this.props.cover}
                         alt=""
                         width={260}
                         height={160}
                     />
                     <h3 className="Media-title">{this.props.title}</h3>
-                    <p className="Media-author">{this.state.author}</p>
+                    <p className="Media-author">{this.props.author}</p>
                 </div>
             </div>
         )
@@ -47,7 +47,7 @@ class Media extends Component {
 }
 
 Media.propTypes = {
-    image: PropTypes.string,
+    cover: PropTypes.string,
     title: PropTypes.string.isRequired,
     author: PropTypes.string,
     type: PropTypes.oneOf(['video', 'audio']),
