0ef440a142541a84511ec2f8eaf037d101185138
Mark Harmsen <mharmsenr@gmail.com>
2019-02-14 11:42:39 -0300

    Handle errors with "componentDidCatch()" in its own component

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -4,6 +4,8 @@ import Categories from '../../categories/components/categories';
 import Related from '../components/related';
 import ModalContainer from '../../widgets/conatiners/modal-container';
 import Modal from '../../widgets/components/modal';
+import HandleError from '../../error/containers/handle-error';
+
 
 class Home extends Component {
     state = {
@@ -21,22 +23,25 @@ class Home extends Component {
     }
     render() {
         return (
-            <HomeLayout>
-                <Related />
-                <Categories categories={this.props.data.categories} 
-                    handleOpenModal = {this.handleOpenModal}
-                />
-                {
-                    this.state.modalVisible &&
-                    <ModalContainer>
-                        <Modal
-                            handleClick={this.handleCloseModal}
-                        >
-                            <h1>Esto es un portal</h1>
-                        </Modal>
-                    </ModalContainer>
-                }
-            </HomeLayout>
+            <HandleError>
+                <HomeLayout>
+                    <Related />
+                    <Categories 
+                        categories={this.props.data.categories} 
+                        handleOpenModal = {this.handleOpenModal}
+                    />
+                    {
+                        this.state.modalVisible &&
+                        <ModalContainer>
+                            <Modal
+                                handleClick={this.handleCloseModal}
+                            >
+                                <h1>Esto es un portal</h1>
+                            </Modal>
+                        </ModalContainer>
+                    }
+                </HomeLayout>
+            </HandleError>
         )
     }
 }
