d95534599f1fe50b7f3453cc45b6cb8a4a38a43a
Mark <mharmsenr@gmail.com>
2019-04-30 18:53:53 -0300

    Add redux, react-redux to dependencies and  createStore to home.js

--- src/entries/home.js
+++ src/entries/home.js
@@ -3,6 +3,24 @@ import { render } from 'react-dom';
 import Home from '../pages/containers/home';
 import data from '../api.json';
 import myplaylist from '../myplaylist.json';
+import { createStore } from 'redux';
+
+const initialSate = {
+    data: {
+        ...data
+    },
+    myplaylist : {
+        ...myplaylist
+    }
+}
+
+const store = createStore(
+    (state) => state,
+    initialSate,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+console.log(store.getState());
 
 
 const homeContainer = document.getElementById('home-container');
